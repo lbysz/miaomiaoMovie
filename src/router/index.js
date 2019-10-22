e501928ff3d2a0fff536b3496254854e799e4eff
xiaomin_ext <4538762@qq.com>
2019-10-22 16:22:06 +0800

    搭建项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+// import Home from "../views/Home.vue";
+import login from "../views/login/login.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: "/", name: "home", component: Home },
-  {
-    path: "/about",
-    name: "about",
-    component: () => import("../views/About.vue")
-  }
+  // { path: "/", name: "home", component: Home },
+  // {
+  //   path: "/about",
+  //   name: "about",
+  //   component: () => import("../views/About.vue")
+  // }
+  { path: "/", redirect: "/login" },
+  { path: "/login", name: "login", component: login }
 ];
 
 const router = new VueRouter({
